57d9a10e57d06a2a02e44e2426b635822a58ce5f
zak905 <zakaria.amine88@gmail.com>
2018-12-11 11:21:00 +0100

    add disqus comments system

--- assets/js/disqus.js
+++ assets/js/disqus.js
@@ -1,8 +1,3 @@
-var disqus_config = function () {
-this.page.url = "{{site.url}}/{{page.url}}";
-this.page.identifier = "{{page.id}}";
-};
-
 (function() { // DON'T EDIT BELOW THIS LINE
 var d = document, s = d.createElement('script');
 s.src = 'https://gwidgets.disqus.com/embed.js';
